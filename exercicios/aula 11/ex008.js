3952a3f358b88a1452e60c27f334f70c245797cb
Pedro Ivo <102565006+pedroivoadv@users.noreply.github.com>
2022-08-16 22:57:55 -0300

    atualização exercicio da aula 11

---
+++ exercicios/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitada. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cindo de segurança!`)
